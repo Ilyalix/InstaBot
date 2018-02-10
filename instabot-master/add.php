c93917c14ebdcd7fd80d1bd7bd6d8f8df94e94b7
Ilyalix <35378965+Ilyalix@users.noreply.github.com>
2018-02-10 23:26:08 +0300

    new

--- instabot-master/add.php
+++ instabot-master/add.php
@@ -17,16 +17,7 @@ $ID = $_GET["ID"];
 
 echo shell_exec('python3 /var/www/html/instaBot-master/instabot-master/botapplication.py -u '.$log.' -p '.$pass.' -w '.$ID.' 2>&1');
 
-// shell_exec('python /var/www/html/instaBot-master/instabot-master/botapplication.py -u '.$log.' -p '.$pass.' -w '.$ID.'');
 
-// echo shell_exec("python /var/www/html/instaBot-master/instabot-master/mkdir.py 2>&1");
-
-
-// $cmd = 'C:\apache\localhost\www\InstaBot\Bot\botapplication.py -u '.$log.' -p '.$pass.' -w '.$ID.'';
-
-//echo "string";
-
-// echo $r;
 
 ?>
 </body>
